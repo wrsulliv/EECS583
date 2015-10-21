2a42ff6be74a87f3cf672b863399f6217691a71c
Will Sullivan <wrsulliv@umich.edu>
2015-10-21 01:26:12 -0400

    Added the actual pass orderings given to me by Reed and I've fixed a number of issues with runeval.sh including newline character and printing issues.  results.scv is populated with the results of running these files.  Notice that 7,8,10,11,16,17 fail often and FloatMM.c fails for all orderings.

--- runeval.sh
+++ runeval.sh
@@ -4,18 +4,18 @@
 ##  Author: Will Sullivan 
 
 #  User defined variables
-echo "--> Setting user defined variables"
+printf "%s\n" "--> Setting user defined variables"
 OUTPUT_FILE=results
 PASS_PERMUTATIONS_FILE=pass_permutations
 
 #  Automatic variables
-echo "Setting the variables..."
+printf "%s\n" "Setting the variables..."
 BENCHMARK_DIR=./benchmarks #Path to the directory holding the c files
 SRC_DIR=${BENCHMARK_DIR}/src
 BIN_DIR=${BENCHMARK_DIR}/bin
 ASM_DIR=${BENCHMARK_DIR}/asm
 
-echo "--> Cleaning the workspace"
+printf "%s\n" "--> Cleaning the workspace"
 rm ./${OUTPUT_FILE}.csv
 
 for SOURCE in ${SRC_DIR}/*.c 
@@ -26,7 +26,7 @@ do
 	SOURCE="${SOURCE%.*}"
 
 	#  Compile the source
-	echo "--> Compiling ${SOURCE}.c"
+	printf "%s\n" "--> Compiling ${SOURCE}.c"
 	clang -emit-llvm -o ${ASM_DIR}/${SOURCE}.bc -c ${SRC_DIR}/${SOURCE}.c
 
 	#  Loop over each entry in the CSV file
@@ -35,20 +35,38 @@ do
 	while read id pass_options
 	do
 		        
-		echo "--> Performing optimization '${id}' with sequence: '${pass_options}'"
-		cmd_str="opt ${pass_options} -disable-output ${ASM_DIR}/${SOURCE}.bc"
+		pass_options_clean=$(printf "%s" ${pass_options} | tr -d '\r\n')
+		#echo "$pass_options" | tr -d '\r\n' | cat -v
+		#echo "hello" | tr -d '4' | cat -v
+		#printf "%s\n" "-->  Performing optimization ${id} with sequence: '${pass_options_clean}'"
+		printf "%s\n" "-->  Generating optimization ${id}"
+		cmd_str="opt ${pass_options_clean} ${ASM_DIR}/${SOURCE}.bc > ${ASM_DIR}/${SOURCE}.opt.bc"
+		#printf "%s\n" ${cmd_str}
 		eval $cmd_str
 
-		echo "--> Generating ${SOURCE}.exe"
-		llc < ${ASM_DIR}/${SOURCE}.bc > ${ASM_DIR}/${SOURCE}.s
-		g++ -o ${BIN_DIR}/${SOURCE}.exe ${ASM_DIR}/${SOURCE}.s
+		printf "%s\n" "--> Generating ${SOURCE}.exe"
+		llc < ${ASM_DIR}/${SOURCE}.opt.bc > ${ASM_DIR}/${SOURCE}.s 
+		g++ -o ${BIN_DIR}/${SOURCE}.exe ${ASM_DIR}/${SOURCE}.s 
 
-		echo "--> Testing ${SOURCE}.c runtime"
-		#\time -o ${PROJECT_DIR}/${OUTPUT_FILE}.csv -a -f "%e" ${BIN_DIR}/${SOURCE}.exe > /dev/null
-		TIMEFORMAT='%3R'
-		elapsed_time=$(time (${BIN_DIR}/${SOURCE}.exe > /dev/null) 2>&1) 
-		echo "${SOURCE},${id},${elapsed_time}" >> ./${OUTPUT_FILE}.csv 
+		#  Check whether or not the file is there
+		if (test -e "${BIN_DIR}/${SOURCE}.exe")
+		then 
+			printf "%s\n" "--> Testing ${SOURCE} runtime"
+			#\time -o ${PROJECT_DIR}/${OUTPUT_FILE}.csv -a -f "%e" ${BIN_DIR}/${SOURCE}.exe > /dev/null
+			TIMEFORMAT='%3R'
+			elapsed_time=$(time (${BIN_DIR}/${SOURCE}.exe > /dev/null 2>&1) 2>&1) 
+			printf "%s\n" "${SOURCE},${id},${elapsed_time}" >> ./${OUTPUT_FILE}.csv 
+		else 
+			printf "Error!"
+			printf "%s\n" "${SOURCE},${id},ERROR" >> ./${OUTPUT_FILE}.csv 
+		fi
 
+
+
+		printf "%s\n" "--> Remove the old files to ensure they are not re-run"
+		rm ${ASM_DIR}/${SOURCE}.opt.bc 2> /dev/null
+		rm ${ASM_DIR}/${SOURCE}.s 2> /dev/null
+		rm ${BIN_DIR}/${SOURCE}.exe 2> /dev/null
 
 	done < ./${PASS_PERMUTATIONS_FILE}.csv
 	IFS=OLDIFS 
